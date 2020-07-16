32c2a636591a96f993715694b7eb52429f33474a
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 14:08:19 -0700

    Added gitignore

--- harvest.py
+++ harvest.py
@@ -10,37 +10,60 @@ class MelonType(object):
                  name):
         """Initialize a melon."""
 
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller #OR 'Bestseller'
+        self.name = name
+
         self.pairings = []
 
-        # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        self.pairings.append(pairing)
 
-        # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
-        # Fill in the rest
-
+        self.code = new_code #overriding
 
 def make_melon_types():
     """Returns a list of current melon types."""
 
     all_melon_types = []
+    #order = code, name, first_harvest, color, is_seedless, is_bestseller
 
-    # Fill in the rest
+    musk = MelonType('musk', 'Muskmelon', 1998, 'green', True, True)
+    musk.add_pairing('mint')
+    all_melon_types.append(musk)
+
+    cas = MelonType('cas', 'Casaba', 2003, 'orange', False, False)
+    cas.add_pairing('strawberries', 'mint')
+    all_melon_types.append(cas)
+
+    cren = MelonType('cren', 'Crenshaw', 1996, 'green', False, False)
+    cren.add_pairing('proscuitto')
+    all_melon_types.append(cren)
+
+    yw = MelonType('yw', 'Yellow Watermelon', 2013, 'yellow', False, True)
+    yw.add_pairing('ice cream')
+    all_melon_types.append(yw)
+
 
     return all_melon_types
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
+    print(f'{self.name} pairs with\n -{self.pairings}')
+
     # Fill in the rest
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
+
 
     # Fill in the rest
 
