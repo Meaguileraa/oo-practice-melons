04dc0a8a5f0032590827335cf2df62634bc10c8f
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-16 16:32:34 -0700

    Melons List

--- harvest.py
+++ harvest.py
@@ -66,14 +66,14 @@ def print_pairing_info(melon_types):
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
     
-    melon_dict = {}
+    melons_by_id = {}
     #keys are codes as strings 
     #values are melon type instance 
 
     for melon in melon_types: 
-        if melon.code not in melon_dict: 
-            melon_dict[melon.code] = melon
-    return melon_dict
+        if melon.code not in melons_by_id: 
+            melons_by_id[melon.code] = melon
+    return melons_by_id
 
 ############
 # Part 2   #
@@ -100,7 +100,25 @@ class Melon(object):
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
-    # Fill in the rest
+    melons = []
+    melons_by_id = make_melon_type_lookup(melon_types)
+
+
+    melon_1 = Melon(melons_by_id['yw'], 8, 7, 2, 'Sheila')
+    melon_2 = Melon(melons_by_id['yw'], 3, 4, 2, 'Sheila')
+    melon_3 = Melon(melons_by_id['yw'], 9, 8, 3, 'Sheila')
+    melon_4 = Melon(melons_by_id['cas'], 10, 6, 35, 'Sheila')
+    melon_5 = Melon(melons_by_id['cren'], 8, 9, 35, 'Michael')
+    melon_6 = Melon(melons_by_id['cren'], 8, 2, 35, 'Michael')
+    melon_7 = Melon(melons_by_id['cren'], 2, 3, 4, 'Michael')
+    melon_8 = Melon(melons_by_id['musk'], 6, 7, 4, 'Michael')
+    melon_9 = Melon(melons_by_id['yw'], 7, 10, 3, 'Sheila')
+
+    melons.extend(melon1, melon_2, melon_3, melon_4, 
+        melon_5, melon_6, melon_7, mleon_8, melon_9)
+
+    return melons
+
 
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
